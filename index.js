2fa607e7073598cac790f58b94a7513427ae1a86
ozhishu <oulazhishu@outlook.com>
2019-10-12 09:53:16 +0800

    添加移动端适配  修改生成位数为12

--- index.js
+++ index.js
@@ -3,13 +3,14 @@
   function buildpass(e) {
     let p1 = document.getElementById('password1').value
     let p2 = document.getElementById('password2').value
-    let encrypted = CryptoJS.MD5(p1 + p2).toString().slice(0, 11).split('');
+    let encrypted = CryptoJS.MD5(p1 + p2).toString().slice(0, 12).split('');
     encrypted[0] = isNaN(encrypted[0]) ? encrypted[0].toUpperCase() : String.fromCharCode(65 + parseInt(encrypted[0], 10))
     encrypted[1] = isNaN(encrypted[1]) ? encrypted[1].toLowerCase() : String.fromCharCode(97 + parseInt(encrypted[1], 10))
     encrypted[7] = '-'
     encrypted[8] = isNaN(encrypted[8]) ? encrypted[8].charCodeAt(0) % 10 : encrypted[8]
     let res = quchong(encrypted.join(''))
     clipboard(res)
+    console.log(res)
     document.getElementById('notify').style.animation = "movenotify 6s 1 linear both"
     e.target.disabled = true
     e.target.style.cssText = "background-color: #f7f7f7;border-color: #dcdee2;color: #c5c8ce;cursor: not-allowed;"
