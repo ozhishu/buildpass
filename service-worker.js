00491632c858418dc370137ac4f598a4c1930a6e
ozhishu <oulazhishu@outlook.com>
2019-12-13 09:45:54 +0800

    service-worker修改精简代码

--- service-worker.js
+++ service-worker.js
@@ -18,25 +18,9 @@
 
   self.addEventListener('install', function (event) {
     self.skipWaiting();
-
     event.waitUntil(
       caches.open(staticCacheName).then(function (cache) {
         return cache.addAll(filesToCache);
-      })
-    );
-  });
-
-  self.addEventListener('activate', function (event) {
-    var cacheWhitelist = [staticCacheName];
-    event.waitUntil(
-      caches.keys().then(function (cacheNames) {
-        return Promise.all(
-          cacheNames.map(function (cacheName) {
-            if (cacheWhitelist.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName);
-            }
-          })
-        );
       })
     );
   });
