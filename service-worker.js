5ed8d708cf1e2e44bf4b69abc19ad8e4cf1a0df4
ozhishu <oulazhishu@outlook.com>
2019-12-13 09:58:27 +0800

    修改相对路径

--- service-worker.js
+++ service-worker.js
@@ -6,14 +6,14 @@
   var staticCacheName = 'pwa-v' + version;
 
   var filesToCache = [
-    '.',
-    'index.htnl',
-    'style/style.css',
-    'style/icon.ico',
-    'style/key.png',
-    'js/clipboard.min.js',
-    'js/index.js',
-    'js/crypto-js.min.js'
+    '/buildpass',
+    '/buildpass/index.html',
+    '/buildpass/style/style.css',
+    '/buildpass/style/icon.ico',
+    '/buildpass/style/key.png',
+    '/buildpass/js/clipboard.min.js',
+    '/buildpass/js/index.js',
+    '/buildpass/js/crypto-js.min.js'
   ];
 
   self.addEventListener('install', function (event) {
